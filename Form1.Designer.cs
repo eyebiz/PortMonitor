a6815bfb3cd7f9f968f6b2f19bebe4053e14f32b
eyebiz <eyebiz@gmail.com>
2026-05-10 15:15:20 +0200

    basic function

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,22 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             tableLayoutPanel1 = new TableLayoutPanel();
-            richTextBox1 = new RichTextBox();
-            panel1 = new Panel();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
             statusStrip1 = new StatusStrip();
             lblStatus = new ToolStripStatusLabel();
+            panel1 = new Panel();
             btnConfig = new Button();
             btnStart = new Button();
             txtPort = new TextBox();
             lblPort = new Label();
-            menuStrip1 = new MenuStrip();
-            fileToolStripMenuItem = new ToolStripMenuItem();
-            helpToolStripMenuItem = new ToolStripMenuItem();
+            richTextBox1 = new RichTextBox();
             tableLayoutPanel1.SuspendLayout();
-            panel1.SuspendLayout();
-            statusStrip1.SuspendLayout();
             menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -64,77 +65,6 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
             tableLayoutPanel1.Size = new Size(989, 590);
             tableLayoutPanel1.TabIndex = 0;
-            // 
-            // richTextBox1
-            // 
-            richTextBox1.Dock = DockStyle.Fill;
-            richTextBox1.Location = new Point(3, 68);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(983, 494);
-            richTextBox1.TabIndex = 0;
-            richTextBox1.Text = "";
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(btnConfig);
-            panel1.Controls.Add(btnStart);
-            panel1.Controls.Add(txtPort);
-            panel1.Controls.Add(lblPort);
-            panel1.Location = new Point(3, 28);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(535, 34);
-            panel1.TabIndex = 1;
-            // 
-            // statusStrip1
-            // 
-            statusStrip1.Items.AddRange(new ToolStripItem[] { lblStatus });
-            statusStrip1.Location = new Point(0, 568);
-            statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(989, 22);
-            statusStrip1.TabIndex = 4;
-            statusStrip1.Text = "statusStrip1";
-            // 
-            // lblStatus
-            // 
-            lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(26, 17);
-            lblStatus.Text = "Idle";
-            // 
-            // btnConfig
-            // 
-            btnConfig.Location = new Point(375, 5);
-            btnConfig.Name = "btnConfig";
-            btnConfig.Size = new Size(75, 23);
-            btnConfig.TabIndex = 3;
-            btnConfig.Text = "Config";
-            btnConfig.UseVisualStyleBackColor = true;
-            // 
-            // btnStart
-            // 
-            btnStart.Location = new Point(202, 5);
-            btnStart.Name = "btnStart";
-            btnStart.Size = new Size(75, 23);
-            btnStart.TabIndex = 2;
-            btnStart.Text = "Start";
-            btnStart.UseVisualStyleBackColor = true;
-            // 
-            // txtPort
-            // 
-            txtPort.Location = new Point(96, 5);
-            txtPort.Name = "txtPort";
-            txtPort.Size = new Size(100, 23);
-            txtPort.TabIndex = 1;
-            txtPort.Text = "8080";
-            // 
-            // lblPort
-            // 
-            lblPort.AutoSize = true;
-            lblPort.Location = new Point(7, 8);
-            lblPort.Name = "lblPort";
-            lblPort.Size = new Size(83, 15);
-            lblPort.TabIndex = 0;
-            lblPort.Text = "Listen on Port:";
             // 
             // menuStrip1
             // 
@@ -157,23 +87,96 @@
             helpToolStripMenuItem.Size = new Size(44, 20);
             helpToolStripMenuItem.Text = "Help";
             // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lblStatus });
+            statusStrip1.Location = new Point(0, 568);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(989, 22);
+            statusStrip1.TabIndex = 4;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(26, 17);
+            lblStatus.Text = "Idle";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnConfig);
+            panel1.Controls.Add(btnStart);
+            panel1.Controls.Add(txtPort);
+            panel1.Controls.Add(lblPort);
+            panel1.Location = new Point(3, 28);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(535, 34);
+            panel1.TabIndex = 1;
+            // 
+            // btnConfig
+            // 
+            btnConfig.Location = new Point(375, 5);
+            btnConfig.Name = "btnConfig";
+            btnConfig.Size = new Size(75, 23);
+            btnConfig.TabIndex = 3;
+            btnConfig.Text = "Config";
+            btnConfig.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(202, 5);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 2;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // txtPort
+            // 
+            txtPort.Location = new Point(96, 5);
+            txtPort.Name = "txtPort";
+            txtPort.Size = new Size(100, 23);
+            txtPort.TabIndex = 1;
+            txtPort.Text = "8080";
+            // 
+            // lblPort
+            // 
+            lblPort.AutoSize = true;
+            lblPort.Location = new Point(7, 8);
+            lblPort.Name = "lblPort";
+            lblPort.Size = new Size(83, 15);
+            lblPort.TabIndex = 0;
+            lblPort.Text = "Listen on Port:";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(3, 68);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(983, 494);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(989, 590);
             Controls.Add(tableLayoutPanel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Port Monitor";
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            statusStrip1.ResumeLayout(false);
-            statusStrip1.PerformLayout();
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,100 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace PortMonitor
 {
     public partial class Form1 : Form
     {
+        private TcpListener? _listener;
+        private bool _isMonitoring = false;
+        private CancellationTokenSource? _cts;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!_isMonitoring)
+            {
+                if (!int.TryParse(txtPort.Text, out int port))
+                {
+                    lblStatus.Text = "Invalid Port Number";
+                    return;
+                }
+
+                try
+                {
+                    _isMonitoring = true;
+                    btnStart.Text = "Stop";
+                    txtPort.Enabled = false;
+                    lblStatus.Text = $"Listening on port {port}...";
+
+                    _cts = new CancellationTokenSource();
+                    _listener = new TcpListener(IPAddress.Any, port);
+                    _listener.Start();
+
+                    // Start the background listening loop
+                    _ = Task.Run(() => ListenLoop(_cts.Token));
+                }
+                catch (Exception ex)
+                {
+                    StopMonitoring();
+                    lblStatus.Text = $"Error: {ex.Message}";
+                }
+            }
+            else
+            {
+                StopMonitoring();
+            }
+        }
+
+        private async Task ListenLoop(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    // Wait for a connection
+                    using var client = await _listener!.AcceptTcpClientAsync(token);
+                    var remoteIp = client.Client.RemoteEndPoint?.ToString() ?? "Unknown";
+
+                    // Capture a bit of data (the "commands" or headers)
+                    byte[] buffer = new byte[1024];
+                    var stream = client.GetStream();
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+                    string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    // Update UI safely
+                    Invoke(() => {
+                        string logEntry = $"[{DateTime.Now:HH:mm:ss}] Connection from {remoteIp}\n";
+                        if (!string.IsNullOrWhiteSpace(data))
+                            logEntry += $"   Data: {data.Replace("\n", " ").Replace("\r", "")}\n";
+
+                        richTextBox1.AppendText(logEntry);
+                        richTextBox1.ScrollToCaret();
+                    });
+
+                    // Optional: Append to file here
+                }
+            }
+            catch (OperationCanceledException) { /* Expected on stop */ }
+            catch (Exception ex)
+            {
+                Invoke(() => lblStatus.Text = $"Listener Error: {ex.Message}");
+            }
+        }
+
+        private void StopMonitoring()
+        {
+            _isMonitoring = false;
+            _cts?.Cancel();
+            _listener?.Stop();
+            btnStart.Text = "Start";
+            txtPort.Enabled = true;
+            lblStatus.Text = "Idle";
         }
     }
 }
