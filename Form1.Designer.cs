9958cf2ac0ff52657e7a09bc3c8d942c99e05135
eyebiz <eyebiz@gmail.com>
2026-05-10 13:07:15 +0200

    ok design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            richTextBox1 = new RichTextBox();
+            panel1 = new Panel();
+            statusStrip1 = new StatusStrip();
+            lblStatus = new ToolStripStatusLabel();
+            btnConfig = new Button();
+            btnStart = new Button();
+            txtPort = new TextBox();
+            lblPort = new Label();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            tableLayoutPanel1.SuspendLayout();
+            panel1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.Controls.Add(menuStrip1, 0, 0);
+            tableLayoutPanel1.Controls.Add(statusStrip1, 0, 3);
+            tableLayoutPanel1.Controls.Add(panel1, 0, 1);
+            tableLayoutPanel1.Controls.Add(richTextBox1, 0, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 25F));
+            tableLayoutPanel1.Size = new Size(989, 590);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(3, 68);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(983, 494);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnConfig);
+            panel1.Controls.Add(btnStart);
+            panel1.Controls.Add(txtPort);
+            panel1.Controls.Add(lblPort);
+            panel1.Location = new Point(3, 28);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(535, 34);
+            panel1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lblStatus });
+            statusStrip1.Location = new Point(0, 568);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(989, 22);
+            statusStrip1.TabIndex = 4;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(26, 17);
+            lblStatus.Text = "Idle";
+            // 
+            // btnConfig
+            // 
+            btnConfig.Location = new Point(375, 5);
+            btnConfig.Name = "btnConfig";
+            btnConfig.Size = new Size(75, 23);
+            btnConfig.TabIndex = 3;
+            btnConfig.Text = "Config";
+            btnConfig.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(202, 5);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 2;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            // 
+            // txtPort
+            // 
+            txtPort.Location = new Point(96, 5);
+            txtPort.Name = "txtPort";
+            txtPort.Size = new Size(100, 23);
+            txtPort.TabIndex = 1;
+            txtPort.Text = "8080";
+            // 
+            // lblPort
+            // 
+            lblPort.AutoSize = true;
+            lblPort.Location = new Point(7, 8);
+            lblPort.Name = "lblPort";
+            lblPort.Size = new Size(83, 15);
+            lblPort.TabIndex = 0;
+            lblPort.Text = "Listen on Port:";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(989, 24);
+            menuStrip1.TabIndex = 5;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(989, 590);
+            Controls.Add(tableLayoutPanel1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
             Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private RichTextBox richTextBox1;
+        private Panel panel1;
+        private Label lblPort;
+        private TextBox txtPort;
+        private Button btnConfig;
+        private Button btnStart;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lblStatus;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
     }
 }
